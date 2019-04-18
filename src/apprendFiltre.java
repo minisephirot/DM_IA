54c440ca589c3a2710cce887a18b8cf59ca3425a
Elliot This <minisephirot@hotmail.fr>
2019-04-18 17:59:46 +0200

    Séparation des fichiers

--- src/apprendFiltre.java
+++ src/apprendFiltre.java
@@ -1,35 +1,33 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
-public class filtreAntiSpam {
+public class apprendFiltre {
 
     private static final boolean debug = false; // editer vrai si on veux voir des infos de debug
 
     public static void main(String args[]) {
         //Init
-        if ( args.length != 3){
-            System.out.println("Utilisation de filtreAntiSpam : 'dossier contenant les spams/ham' 'nb spam testé' 'nb ham testé'");
+        if ( args.length != 4){
+            System.out.println("Utilisation de apprendFiltre : 'nom du fichier sortie' 'dossier contenant la base d'apprentissage' 'nb de spam de la base d'apprentissage' 'nb de ham de la base d'apprentissage'");
             System.exit(1);
         }
-        Scanner sc = new Scanner(System.in);  // Create a Scanner object
 
         //Dictionnaire
         System.out.println("Chargement du dictionnaire...");
-        String[] dictionnaire = filtreAntiSpam.charger_dictionnaire();
+        String[] dictionnaire = apprendFiltre.charger_dictionnaire();
         System.out.println("Dictionnaire chargé. "+dictionnaire.length+" mots ont été enregistrés.");
         if (debug) System.out.println("Liste des mots :"+Arrays.asList(dictionnaire)+"\n");
 
         //Apprentissage
-        double nbham = Objects.requireNonNull(new File("baseapp/ham").list()).length;
-        double nbspam = Objects.requireNonNull(new File("baseapp/spam").list()).length;
-        System.out.println("-Combien de SPAM de la base d'apprentissage ? Min = 1, Max = "+nbspam);
-        nbspam = sc.nextInt();
-        System.out.println("-Combien de HAM de la base d'apprentissage ? Min = 1, Max = "+nbham);
-        nbham = sc.nextInt();
-
+        double nbham = Objects.requireNonNull(new File(args[1]+"/ham").list()).length;
+        double nbspam = Objects.requireNonNull(new File(args[1]+"/spam").list()).length;
+        if(Integer.parseInt(args[2]) < nbspam && Integer.parseInt(args[3]) < nbham ){
+            nbspam = Integer.parseInt(args[2]);
+            nbham = Integer.parseInt(args[3]);
+        }
 
         System.out.println("Apprentissage...");
+
         //Création des b_spam et b_ham
         HashMap<String,Double> probaSpam = new HashMap<>(dictionnaire.length); //On sait que l'on va utiliser uniquement les mots du dictionnaire.
         for (String mot : dictionnaire) {
@@ -42,21 +40,18 @@ public class filtreAntiSpam {
         if (debug) {
             System.out.println("Init des proba de spam :"+Collections.singletonList(probaSpam));
             System.out.println("Init des proba de ham :"+Collections.singletonList(probaHam));
-            System.out.println("\nPour couper les mots et leurs ponctuations :");
-            System.out.println("Ceci est un test, il n'est pas important");
-            System.out.println(Arrays.asList("Ceci est un test, il n'est pas important".split("[\\s\\p{Punct}]+"))+"\n");
         }
 
-        String apprentissage = "baseapp";
+        String apprentissage = args[1];
         //Apprentissage des SPAM:
         for (int i = 0; i < nbspam; i++) {
-            HashMap<String,Double> vecteurx =  filtreAntiSpam.lire_message(dictionnaire,new File(apprentissage+"/spam/"+i+".txt"));
-            filtreAntiSpam.mergeValues(probaSpam,vecteurx);
+            HashMap<String,Double> vecteurx =  apprendFiltre.lire_message(dictionnaire,new File(apprentissage+"/spam/"+i+".txt"));
+            apprendFiltre.mergeValues(probaSpam,vecteurx);
         }
         //Apprentissage des HAM:
         for (int i = 0; i < nbham; i++) {
-            HashMap<String,Double> vecteurx =  filtreAntiSpam.lire_message(dictionnaire,new File(apprentissage+"/ham/"+i+".txt"));
-            filtreAntiSpam.mergeValues(probaHam,vecteurx);
+            HashMap<String,Double> vecteurx =  apprendFiltre.lire_message(dictionnaire,new File(apprentissage+"/ham/"+i+".txt"));
+            apprendFiltre.mergeValues(probaHam,vecteurx);
         }
         if (debug){
             System.out.println("Effectif des mots après lecture des spam :"+Collections.singletonList(probaSpam));
@@ -64,8 +59,8 @@ public class filtreAntiSpam {
         }
         //On a compté l'effectif d'apparition des mots dans les 2 catégories, on doit maintenant diviser ces effectifs par
         //Leurs nombre respectif de spam/ham avec +2 pour les spam car nous lissons ces probabilitées.
-        filtreAntiSpam.effectifToFrequency(probaSpam,nbspam+2);
-        filtreAntiSpam.effectifToFrequency(probaHam,nbham);
+        apprendFiltre.effectifToFrequency(probaSpam,nbspam+2);
+        apprendFiltre.effectifToFrequency(probaHam,nbham);
         if (debug){
             System.out.println("\nFrequence d'apparition des mots (spam) :"+Collections.singletonList(probaSpam));
             System.out.println("Frequence d'apparition des mots (ham) :"+Collections.singletonList(probaHam));
@@ -76,66 +71,23 @@ public class filtreAntiSpam {
         double pYegalHam = 1d - pYegalSpam;
         if (debug) System.out.println("Probabilité qu'un message soit un spam vs Probabilité qu'un message soit un ham = "+ pYegalSpam+" contre "+ pYegalHam);
 
-        //Tests
-        double nbtestspam = Integer.parseInt(args[1]);
-        double nbtestham = Integer.parseInt(args[2]);
-        System.out.println("\nTests...");
-        // p(X = x) que l'on obtiens via les probabilitées totales :
-        // p(X = x) = P(X = x , Y = SPAM) + P(X = x , Y = HAM)
-        // Et P(X = x , Y = SPAM) = P(X = x | Y = SPAM) * P(Y = SPAM)
-        // Et P(X = x , Y = HAM) = P(X = x | Y = HAM) * P(Y = HAM)
-        // Enfin, P(X = x | Y = SPAM ou HAM) = les formule qui sont disponibles le diapo sur arche, diapo 52
-        double nberreur = 0d;
-        for (int i = 0; i < nbtestspam ; i++) {
-            HashMap<String,Double> vecteurx =  filtreAntiSpam.lire_message(dictionnaire,new File(args[0]+"/spam/"+i+".txt"));
-            double pXegalxSachantYegalSpam = getPdeXsachantYegalSpamOuHam(probaSpam,vecteurx);
-            double pXegalxSachantYegalHam = getPdeXsachantYegalSpamOuHam(probaHam,vecteurx);
-            double pDeXetYegalSpam = pXegalxSachantYegalSpam * pYegalSpam;
-            double pDeXetYegalHam = pXegalxSachantYegalHam * pYegalHam;
-            double pDeXegalx = pDeXetYegalHam + pDeXetYegalSpam;
+        System.out.println("Serialization...");
+        Classifieur classifieur = new Classifieur(probaSpam, probaHam, pYegalSpam, pYegalHam);
+        File fichier =  new File(args[0] + ".ser") ;
 
-            double pDeYegalSpamSachantXegalx = (1d/pDeXegalx) * pYegalSpam * pXegalxSachantYegalSpam ;
-            double pDeYegalHamSachantXegalx = (1d/pDeXegalx) * pYegalHam * pXegalxSachantYegalHam;
-
-            boolean isSpam = filtreAntiSpam.isSpam(pDeYegalSpamSachantXegalx,pDeYegalHamSachantXegalx);
-            System.out.print("SPAM "+i+" : P(Y=SPAM | X=x) = "+pDeYegalSpamSachantXegalx+", P(Y=HAM | X=x) = "+pDeYegalHamSachantXegalx+" => identifié comme un ");
-            if (isSpam){
-                System.out.print("SPAM !\n");
-            }else{
-                System.out.print("HAM ! *Erreur*\n");
-                nberreur++;
-            }
+        // ouverture d'un flux sur un fichier
+        ObjectOutputStream oos;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream(fichier));
+            // sérialization de l'objet
+            oos.writeObject(classifieur) ;
+            oos.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.exit(1);
         }
-        double erreurSpam = (nberreur/nbtestspam)*100d*10/10;
-
-        nberreur = 0d;
-        for (int i = 0; i < nbtestham ; i++) {
-            HashMap<String,Double> vecteurx =  filtreAntiSpam.lire_message(dictionnaire,new File(args[0]+"/ham/"+i+".txt"));
-            double pXegalxSachantYegalSpam = getPdeXsachantYegalSpamOuHam(probaSpam,vecteurx);
-            double pXegalxSachantYegalHam = getPdeXsachantYegalSpamOuHam(probaHam,vecteurx);
-            double pDeXetYegalSpam = pXegalxSachantYegalSpam * pYegalSpam;
-            double pDeXetYegalHam = pXegalxSachantYegalHam * pYegalHam;
-            double pDeXegalx = pDeXetYegalHam + pDeXetYegalSpam;
-
-            double pDeYegalSpamSachantXegalx = (1d/pDeXegalx) * pYegalSpam * pXegalxSachantYegalSpam ;
-            double pDeYegalHamSachantXegalx = (1d/pDeXegalx) * pYegalHam * pXegalxSachantYegalHam;
-
-            boolean isSpam = filtreAntiSpam.isSpam(pDeYegalSpamSachantXegalx,pDeYegalHamSachantXegalx);
-            System.out.print("HAM "+i+" : P(Y=SPAM | X=x) = "+pDeYegalSpamSachantXegalx+", P(Y=HAM | X=x) = "+pDeYegalHamSachantXegalx+" => identifié comme un ");
-            if (isSpam){
-                System.out.print("SPAM ! *Erreur*\n");
-                nberreur++;
-            }else{
-                System.out.print("HAM !\n");
-            }
-        }
-
-        double erreurHam = (nberreur/nbtestham)*100d*10/10;
-        double nbtotaltest = nbtestham + nbtestspam;
-        double erreurTotale = (erreurHam * (nbtestham/nbtotaltest)) + (erreurSpam * (nbtestspam/nbtotaltest));
-        System.out.println("Erreur de test sur les "+nbtestspam+" SPAM : "+erreurSpam+" %");
-        System.out.println("Erreur de test sur les "+nbtestham+" HAM : "+erreurHam+" %");
-        System.out.println("Erreur totale sur les "+nbtotaltest+" mails : "+erreurTotale+" %");
+        System.out.println("Creation de "+args[0] + ".ser terminée");
     }
 
     //Methode qui return a > b pour de très petit nombres
